e9a20b139f1879c990e9b98fbb9063c0561933ac
ticketoive <helohelo1226@gmail.com>
2026-08-15 16:49:01 +0900

    Update namesy.js

--- namesy.js
+++ namesy.js
@@ -1,1 +1,15 @@
+// ==UserScript==
+// @name namesy
+// @match https://ticket.tv-asahi.co.jp/tp/ticket/access/wnCaGUd9adyd22IbRVw5ja602l7XypjovY1DqA3UYlWzqiLk2P6CChhppEIByapP
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  const el = document.querySelector('.gen_code');
+  if (el) {
+    el.innerHTML = el.innerHTML.replace('小林  琢弥', '齋藤 将弘');
+  }
+
+})();
 
