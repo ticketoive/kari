addf00938be03c2adcaaed38b4f94459685523ed
ticketoive <helohelo1226@gmail.com>
2026-08-15 14:39:12 +0900

    Update match URL in UserScript metadata

--- name2.js
+++ name2.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name name
-// @match https://ticket.tv-asahi.co.jp/tp/ticket/access/*
+// @match https://ticket.tv-asahi.co.jp/tp/ticket/access/5vhYchobmoSl1RIEUpBVWEEIOEYVXVRNJwkUYGN85nsg1BovKpo2bbbDro96I3RD
 // ==/UserScript==
 
 (function () {
