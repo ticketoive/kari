0be457d87c23d52d4b249a2e47753d212eece3bf
ticketoive <helohelo1226@gmail.com>
2026-08-21 13:10:43 +0900

    Update diveurl.js

--- diveurl.js
+++ diveurl.js
@@ -1,6 +1,23 @@
 // ==UserScript==
-// @name ChangeTabTitle
-// @match https://ticketoive.github.io/com1/5.html
+// @name FakeAddressBar ticketdive.com (partial match)
+// @match https://ticketoive.github.io/*
+// @run-at document-start
 // ==/UserScript==
 
-document.title = "ticketdive.com";
+(function () {
+  'use strict';
+
+  const url = location.href;
+
+  // ★ 部分一致で判定
+  if (url.includes("/com/")) {
+
+    // 本物のパスをそのまま使って偽装
+    const path = url.replace("https://ticketoive.github.io", "");
+    const fakeUrl = "https://ticketdive.com" + path;
+
+    history.replaceState(null, "", fakeUrl);
+  }
+
+})();
+
