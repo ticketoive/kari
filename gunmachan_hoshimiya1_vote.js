22010960b39e2972cc4d2c22dbf728c1513d029d
ticketoive <helohelo1226@gmail.com>
2026-08-31 15:59:44 +0900

    Create gunmachan_hoshimiya1_vote.js

---
+++ gunmachan_hoshimiya1_vote.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name        星宮美咲 自動投票
+// @match       https://gunmachan-idolfes.com/votes/gunmachan_official_supporter2027/list
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function clickYodaButton() {
+        const cards = document.querySelectorAll('.vote-list-item-card');
+        for (const card of cards) {
+            const title = card.querySelector('h3');
+            if (title && title.textContent.trim() === '星宮美咲') {
+                const button = card.querySelector('.vote-list-item-button button');
+                if (button) {
+                    console.log('投票します');
+                    button.click();
+
+                    // 5秒後に閉じる
+                    setTimeout(() => {
+                        console.log('5秒経過 → タブを閉じます');
+                        window.close();
+                    }, 5000);
+                }
+                return;
+            }
+        }
+    }
+
+    window.addEventListener('load', () => {
+        setTimeout(clickYodaButton, 1200);
+    });
+})();
