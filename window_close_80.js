6458e2e1876d1aaa1950d19abb0a23088251bb09
ticketoive <helohelo1226@gmail.com>
2026-09-01 20:21:32 +0900

    Update window_close_80.js

--- window_close_80.js
+++ window_close_80.js
@@ -1,12 +1,13 @@
 // ==UserScript==
-// @name　80秒でブラウザ閉じる
-// @match https://gunmachan-idolfes.com/*
+// @name    80秒でブラウザ閉じる
+// @match   https://gunmachan-idolfes.com/*
+// @grant   window.close
 // ==/UserScript==
 (function () {
-use strict';
+    'use strict';
 
-setTimeout(() => {
-console.log('80秒経過。タブを閉じます。');
-window.close();
-}, 80000);
+    setTimeout(() => {
+        console.log('80秒経過。タブを閉じます。');
+        window.close();
+    }, 80000);
 })();
