d7f3ece89ed8e636f88c7d4a25dbcad69595af98
ticketoive <helohelo1226@gmail.com>
2026-09-01 20:10:29 +0900

    Add script to close browser tab after 80 seconds

---
+++ window_close_80.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name　80秒でブラウザ閉じる
+// @match https://gunmachan-idolfes.com/*
+// ==/UserScript==
+(function () {
+use strict';
+
+setTimeout(() => {
+console.log('80秒経過。タブを閉じます。');
+window.close();
+}, 80000);
+})();
