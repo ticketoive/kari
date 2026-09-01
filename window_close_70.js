6b9658c0a21481ac5f6ba181bf3fb785951cff4d
ticketoive <helohelo1226@gmail.com>
2026-09-01 20:20:34 +0900

    Update window_close_70.js

--- window_close_70.js
+++ window_close_70.js
@@ -1,12 +1,13 @@
 // ==UserScript==
-// @name　70秒でブラウザ閉じる
-// @match https://gunmachan-idolfes.com/*
+// @name    70秒でブラウザ閉じる
+// @match   https://gunmachan-idolfes.com/*
+// @grant   window.close
 // ==/UserScript==
 (function () {
-use strict';
+    'use strict';
 
-setTimeout(() => {
-console.log('70秒経過。タブを閉じます。');
-window.close();
-}, 70000);
+    setTimeout(() => {
+        console.log('70秒経過。タブを閉じます。');
+        window.close();
+    }, 70000);
 })();
